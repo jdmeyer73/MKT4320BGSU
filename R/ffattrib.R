ab6e2cea672245064d4932f696ab029ebe2d55aa
Jeffrey Meyer <jdmeyer@bgsu.edu>
2026-01-24 11:07:28 -0500

    Add ffattrib data used in lecture materials

---
+++ R/ffattrib.R
@@ -0,0 +1,22 @@
+#' Fast Food Attribute Perception Data
+#'
+#' Attribute perception ratings related to fast food restaurants in Bowling
+#' Green, Ohio. Collected from BGSU students.
+#'
+#' @docType data
+#' @format A data frame with 320 rows and 10 variables:
+#' \describe{
+#'   \item{id}{Integer. Respondent identifier.}
+#'   \item{rest}{Factor. Fast food restaurant name.}
+#'   \item{taste}{Integer. Perceptions of restaurant taste (1-low to 5-high scale).}
+#'   \item{speed}{Integer. Perceptions of restaurant speed (1-low to 5-high scale).}
+#'   \item{value}{Integer. Perceptions of restaurant value (1-low to 5-high scale).}
+#'   \item{variety}{Integer. Perceptions of restaurant menu variety (1-low to 5-high scale).}
+#'   \item{clean}{Integer. Perceptions of restaurant cleanliness (1-low to 5-high scale).}
+#'   \item{conv}{Integer. Perceptions of restaurant convenience (1-low to 5-high scale).}
+#'   \item{healhty}{Integer. Perceptions of restaurant healthiness (1-low to 5-high scale).}
+#'   \item{pref}{Integer. Restaurant preference (1-low to 5-high scale).}
+#' }
+#' @source Data from MKT 3200 classes.
+#' @keywords datasets
+"ffattrib"
