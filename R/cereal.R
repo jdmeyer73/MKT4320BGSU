e2548ce6a3ee0802c5d4173716c963dc73146c29
Jeffrey Meyer <jdmeyer@bgsu.edu>
2026-04-10 11:40:40 -0400

    Update advtsales, add cereal and cheese

---
+++ R/cereal.R
@@ -0,0 +1,17 @@
+#' Cereal Price and Sales
+#'
+#' Price and sales data for one cereal brand over 380 weeks.
+#'
+#' @details
+#' Used for modeling price response model.
+#'
+#' @docType data
+#' @format A data frame with 380 rows and 3 variables:
+#' \describe{
+#'   \item{week}{Integer. Week number.}
+#'   \item{sold}{Integer. Weekly sales (in 000s).}
+#'   \item{price}{Numeric. Price in dollars.}
+#' }
+#' @source Unknown.
+#' @keywords datasets
+"cereal"
---
+++ R/cheese.R
@@ -0,0 +1,19 @@
+#' Cheese Price, Advertising, and Sales
+#'
+#' Price, advertising and sales data for one cheese brand across 20 cities and 3 stores.
+#'
+#' @details
+#' Used for modeling price and advertising response model.
+#'
+#' @docType data
+#' @format A data frame with 1863 rows and 5 variables:
+#' \describe{
+#'   \item{vol}{Integer. Weekly volume sales.}
+#'   \item{price}{Numeric. Price in dollars.}
+#'   \item{store}{Character. Store name (KROGER, FOOD LION, WINN DIXIE.}
+#'   \item{loc}{Character. City name.}
+#'   \item{adv}{Numeric. Index of advertising spending.}
+#' }
+#' @source Unknown.
+#' @keywords datasets
+"cheese"
